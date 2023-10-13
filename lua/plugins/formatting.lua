e705eb4ebec503349d6ca4c7b9c0ff3bf9cb2496
Falcon <falcon@zigtag.dev>
2023-10-12 22:45:40 -0700

    chore: comment out invalid code

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,4 +1,15 @@
 return {
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       javascript = { "eslint_d" },
+  --       javascriptreact = { "eslint_d" },
+  --       typescript = { "eslint_d" },
+  --       typescriptreact = { "eslint_d" },
+  --     },
+  --   },
+  -- },
   {
     "neovim/nvim-lspconfig",
     opts = {
