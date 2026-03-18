8bec699081e9bc60c936ef67379b753b6cb7d08e
Falcon <falcon@zigtag.dev>
2026-03-17 23:43:39 -0700

    chore: make catppuccin black finally

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,4 +5,17 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "catppuccin/nvim",
+    opts = {
+      color_overrides = {
+        mocha = {
+          text = "#ffffff",
+          base = "#000000",
+          mantle = "#010101",
+          crust = "#020202",
+        },
+      },
+    },
+  },
 }
